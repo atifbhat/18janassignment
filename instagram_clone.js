ad2c85909fd3168c0ffb912f9f8f2cda109b0889
Atif Bhat <94857007+atifbhat@users.noreply.github.com>
2022-01-23 17:40:05 +0530

    Add files via upload

---
+++ instagram_clone.js
@@ -0,0 +1,21 @@
+//  document.getElementById("main_img").addEventListener("click", function() {
+//      document.querySelector(".fa-heart-o").style.backgroundColor = red;
+//  });
+
+
+
+
+//  document.querySelector('.fa-heart-o').onclick = changeColor;
+
+//  function changeColor() {
+//      document.button.style.backgroundColor = "red";
+
+
+//  }
+
+
+let changeColor = function(icon) {
+
+    icon.classList.toggle('fa-facebook');
+    icon.remove.classList.toggle('fa-heart-o')
+}
